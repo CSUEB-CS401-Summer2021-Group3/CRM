791ef0b37875fc7d2270908fc5770cb1f11651e2
Nicholas Krone <nkrone2@horizon.csueastbay.edu>
2021-07-12 11:30:30 -0700

    Message implementation

---
+++ src/main/java/edu/cs401group3/crm/common/message/AuthenticationMessage.java
@@ -0,0 +1,48 @@
+package edu.cs401group3.crm.common.message;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthenticationMessage implements Message {
+	private final String type = "authentication";
+	private String status;
+	Map<String, String> credentials;
+	
+	public AuthenticationMessage(String user, String password) {
+		credentials = new HashMap<String, String>();
+		credentials.put("user", user);
+		credentials.put("password", password);
+		status = "pending";
+	}
+	
+	public AuthenticationMessage(Map<String, String> credentials) {
+		this.credentials = credentials;
+		status = "pending";
+	}
+
+	@Override
+	public String getType() {
+		return type;
+	}
+
+	@Override
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String getStatus() {
+		return status;
+	}
+
+	@Override
+	public void setContent(Map<String, String> credentials) {
+		this.credentials = credentials;
+	}
+
+	@Override
+	public Map<String, String> getContent() {
+		return credentials;
+	}
+
+}
