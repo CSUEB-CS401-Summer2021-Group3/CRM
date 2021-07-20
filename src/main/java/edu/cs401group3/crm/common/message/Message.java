ac0662942fefbd626a0d7985ba3fd37191666b7d
Nicholas Krone <nkrone2@horizon.csueastbay.edu>
2021-07-19 20:18:28 -0700

    set type, status and setters/getters

--- src/main/java/edu/cs401group3/crm/common/message/Message.java
+++ src/main/java/edu/cs401group3/crm/common/message/Message.java
@@ -4,38 +4,33 @@ import java.io.Serializable;
 import java.util.Map;
 
 public class Message implements MessageInterface, Serializable{
-	private String type;
+	private final String type = "generic";
 	private String status;
 	private Map<String, String> content;
 	
 	@Override
 	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return type;
 	}
 
 	@Override
 	public void setStatus(String status) {
-		// TODO Auto-generated method stub
-		
+		this.status = status;
 	}
 
 	@Override
 	public String getStatus() {
-		// TODO Auto-generated method stub
-		return null;
+		return status;
 	}
 
 	@Override
 	public void setContent(Map<String, String> content) {
-		// TODO Auto-generated method stub
-		
+		this.content = content;	
 	}
 
 	@Override
 	public Map<String, String> getContent() {
-		// TODO Auto-generated method stub
-		return null;
+		return content;
 	}
 
 }
