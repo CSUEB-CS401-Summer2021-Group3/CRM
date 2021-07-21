6a0b5fb73cf8df133438e34b32192d51d5106c36
MapleStar996 <809080592@qq.com>
2021-07-21 16:19:27 -0700

    test

---
+++ src/main/java/edu/cs401group3/crm/common/SHA256Test.java
@@ -0,0 +1,14 @@
+package edu.cs401group3.crm.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SHA256Test {
+
+	@Test
+	void test() {
+		assertEquals("a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3","1");
+	}
+
+}
