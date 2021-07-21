f3c9a8dce306c53c073940d323d50158fe099375
MapleStar996 <809080592@qq.com>
2021-07-21 14:34:50 -0700

    generate salt

---
+++ src/main/java/edu/cs401group3/crm/client/saltTest.java
@@ -0,0 +1,15 @@
+package edu.cs401group3.crm.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class saltTest {
+
+	@Test
+	void test() {
+		JunitTesting test = new JunitTesting();
+		
+	}
+
+}
