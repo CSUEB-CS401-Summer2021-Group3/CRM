19a989c0326a668fd502b8b8c2ab053f5dced6c6
Nicholas Krone <nkrone2@horizon.csueastbay.edu>
2021-07-23 04:19:38 -0700

    Run adduser command

--- src/main/java/edu/cs401group3/crm/commands/CommandProcessor.java
+++ src/main/java/edu/cs401group3/crm/commands/CommandProcessor.java
@@ -1,9 +1,19 @@
 package edu.cs401group3.crm.commands;
 
+import edu.cs401group3.crm.commands.user.AddUser;
+import edu.cs401group3.crm.commands.user.User;
+import edu.cs401group3.crm.common.message.CommandMessage;
+
 public class CommandProcessor {
-	public void processCommand(Commands command) {
+	public void processCommand(CommandMessage message) {
+		Commands command = message.getCommand();
 		switch (command) {
-			case ADD_USER: break;
+			case ADD_USER: 
+				System.out.println("ComProc: Add"); 
+				System.out.println(message.getContent().get("user").toString());
+				AddUser add = new AddUser((User) message.getContent().get("user"));
+				add.execute();
+				break;
 			case DELETE_USER: break;
 			case EDIT_USER: break;
 			
