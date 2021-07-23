a9610e950316812de415a26742923b26aef91800
Nicholas Krone <nkrone2@horizon.csueastbay.edu>
2021-07-22 21:09:57 -0700

    Revert

--- src/main/java/edu/cs401group3/crm/common/message/AuthenticationMessage.java
+++ src/main/java/edu/cs401group3/crm/common/message/AuthenticationMessage.java
@@ -6,16 +6,44 @@ import java.util.Map;
 
 public class AuthenticationMessage extends Message implements Serializable{
 	private final String type = "authentication";
-
+	private String status;
+	Map<String, Object> credentials;
+	
 	public AuthenticationMessage(String username, String password) {
-		content = new HashMap<String, Object>();
-		content.put("username", username);
-		content.put("password", password);
+		credentials = new HashMap<String, Object>();
+		credentials.put("username", username);
+		credentials.put("password", password);
 		status = "pending";
 	}
 	
-	public AuthenticationMessage(Map<String, Object> content) {
-		this.content = content;
+	public AuthenticationMessage(Map<String, Object> credentials) {
+		this.credentials = credentials;
 		status = "pending";
 	}
+
+	@Override
+	public String getType() {
+		return type;
+	}
+
+	@Override
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String getStatus() {
+		return status;
+	}
+
+	@Override
+	public void setContent(Map<String, Object> credentials) {
+		this.credentials = credentials;
+	}
+
+	@Override
+	public Map<String, Object> getContent() {
+		return credentials;
+	}
+
 }
--- src/main/java/edu/cs401group3/crm/common/message/CommandMessage.java
+++ src/main/java/edu/cs401group3/crm/common/message/CommandMessage.java
@@ -7,11 +7,37 @@ import edu.cs401group3.crm.commands.Commands;
 
 public class CommandMessage extends Message implements Serializable {
 	private final String type = "command";
+	private String status;
 	private Commands command;
 	
 	public CommandMessage(Commands command, Map<String, Object> content) {
 		this.command = command;
 		this.content = content;
 		status = "pending";
+	}
+	
+	@Override
+	public String getType() {
+		return type;
+	}
+
+	@Override
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String getStatus() {
+		return status;
+	}
+
+	@Override
+	public void setContent(Map<String, Object> content) {
+		this.content = content;
+	}
+
+	@Override
+	public Map<String, Object> getContent() {
+		return content;
 	}
 }
