85446d0d4d3b575ce57a481476363c8c303bdea5
Nicholas Krone <nkrone2@horizon.csueastbay.edu>
2021-07-22 20:22:48 -0700

    Command modules

---
+++ src/main/java/edu/cs401group3/crm/commands/CommandProcessor.java
@@ -0,0 +1,27 @@
+package edu.cs401group3.crm.commands;
+
+public class CommandProcessor {
+	public void processCommand(Commands command) {
+		switch (command) {
+			case ADD_USER: break;
+			case DELETE_USER: break;
+			case EDIT_USER: break;
+			
+			case ADD_CLIENT: break;
+			case DELETE_CLIENT: break;
+			case EDIT_CLIENT: break;
+			
+			case ADD_RECORD: break;
+			case DELETE_RECORD: break;
+			case EDIT_RECORD: break;
+			
+			case ADD_GROUP: break;
+			case DELETE_GROUP: break;
+			case EDIT_GROUP: break;
+			
+			case IMPORT: break;
+			case EXPORT: break;
+			case SAVE: break;
+		}
+	}
+}
