7c799e59436232248b12b632c4cae3f995ae98e3
Jacki Zhong <49500744+dargonjz@users.noreply.github.com>
2021-07-24 13:18:03 -0700

    Add files via upload

--- zCRM_GUI.java
+++ zCRM_GUI.java
@@ -9,36 +9,195 @@ import javax.swing.event.ListSelectionListener;
 import java.io.File;
 import java.io.IOException;
 import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.InputVerifier;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
 
 import java.awt.event.*;
 
 public class zCRM_GUI {
 		
 	private static JFrame frame;
-		
+	static JPanel userTitle;
+	static JPanel actionPanel;
+	static JPanel actionInputPanel;
+	static Box infoBox;
+	static JTextField newUserName;
+	static JTextField newPassword;
+	
 	private static void adminGUI() {
 		frame = new JFrame("CRM GUI");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		        
 		frame.setLayout(new FlowLayout());
-		frame.setSize(300, 300);
+		frame.setSize(700, 500);
 
-		JPanel p = new JPanel(new BorderLayout()); 	
-		JPanel t = new JPanel();
+		userTitle = new JPanel(new BorderLayout()); 	
+		actionPanel = new JPanel();
+		actionInputPanel = new JPanel(new BorderLayout());
 		
-		t.setLayout(new BoxLayout(t, BoxLayout.Y_AXIS));  
 		
-		frame.add(p);
-		p.setBorder(BorderFactory.createCompoundBorder(
+		actionPanel.setLayout(new BoxLayout(actionPanel, BoxLayout.Y_AXIS));  
+		actionInputPanel.setLayout(new BoxLayout(actionInputPanel, BoxLayout.X_AXIS));
+		
+		frame.add(userTitle);
+		frame.add(actionInputPanel);
+		
+		userTitle.setBorder(BorderFactory.createCompoundBorder(
 	            BorderFactory.createTitledBorder("Welcome Admin"),
-	            BorderFactory.createEmptyBorder(15, 15, 15, 15))
-	        );
+	            BorderFactory.createEmptyBorder(15, 15, 15, 15)));
+				
 		
-		    
-		Box infoBox = Box.createVerticalBox();
-    
-		Box buttonBox = Box.createVerticalBox();
+		JPanel panel = new JPanel();
+		JPanel panel2 = new JPanel();
+		panel.setSize(50, 50);
+		
+		GridLayout gl = new GridLayout(4,2); // 4 rows, 2 columns
+		panel2.setLayout(gl);
+		JButton ok = new JButton("Ok");
+		JButton cancel = new JButton("Cancel");
+		
+		JRadioButton jRadioButtonSP, jRadioButtonM, jRadioButtonA;
+		JButton jButtonSaleperson, jButtonManager, jButtonAdmin;
+		ButtonGroup G1;
+		JLabel labelSalePerson, labelManager, labelAdmin;
+		
+		jRadioButtonSP = new JRadioButton();
+		jRadioButtonM = new JRadioButton();
+		jRadioButtonA = new JRadioButton();
+		
+		labelSalePerson = new JLabel("Sale Person");
+		labelManager = new JLabel("Manager");
+		labelAdmin = new JLabel("Admin");
+		G1 = new ButtonGroup();
+	     
+	    jRadioButtonSP.setBounds(120, 30, 120, 50);
+	    panel.add(jRadioButtonSP);
+	    panel.add(labelSalePerson);
+	    G1.add(jRadioButtonSP);
+	   
+	    jRadioButtonM.setBounds(250, 30, 80, 50);
+	    panel.add(jRadioButtonM);
+	    panel.add(labelManager);
+	    G1.add(jRadioButtonM);
+	   
+	    jRadioButtonA.setBounds(450, 30, 80, 50);
+	    panel.add(jRadioButtonA);
+	    panel.add(labelAdmin);
+	    G1.add(jRadioButtonA);
 
+//	    String accountType = "";
+//        ok.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e){
+//                if (jRadioButtonSP.isSelected())
+//                	accountType = "salePerson";
+//                else if (jRadioButtonM.isSelected())
+//                	accountType = "Manager";
+//                else if (jRadioButtonA.isSelected())
+//                    accountType = "Admin";
+//                else
+//                	JOptionPane.showMessageDialog(frame, accountType);
+//                	//accountType = "NO Button selected";
+//            }
+//        });
+	    
+
+
+		JLabel userNameLabel = new JLabel("User Name:");
+		newUserName = new JTextField(15);
+		
+		//userNameLabel.setBounds(200, 3000, 1050, 500);
+		panel2.add(userNameLabel);
+		panel2.add(newUserName);
+
+//		newUserName.addFocusListener(new FocusListener() {
+//		      public void focusGained(FocusEvent e) {
+//		          displayMessage("Focus gained", e);
+//		        }
+//
+//		        public void focusLost(FocusEvent e) {
+//		          displayMessage("Focus lost", e);
+//		        }
+//
+//		        void displayMessage(String prefix, FocusEvent e) {
+//		          System.out.println(prefix
+//		              + (e.isTemporary() ? " (temporary):" : ":")
+//		              + e.getComponent().getClass().getName()
+//		              + "; Opposite component: "
+//		              + (e.getOppositeComponent() != null ? e.getOppositeComponent().getClass().getName()
+//		                  : "null"));
+//		        }
+//
+//		      });
+		String t = "";
+		DocumentListener documentListener = new DocumentListener() {
+		      public void changedUpdate(DocumentEvent documentEvent) {
+		        printIt(documentEvent);
+		      }
+
+		      public void insertUpdate(DocumentEvent documentEvent) {
+		        printIt(documentEvent);
+		      }
+
+		      public void removeUpdate(DocumentEvent documentEvent) {
+		        printIt(documentEvent);
+		      }
+
+		      private void printIt(DocumentEvent documentEvent) {
+		        DocumentEvent.EventType type = documentEvent.getType();
+		        String typeString = null;
+		        if (type.equals(DocumentEvent.EventType.CHANGE)) {
+		          typeString = "Change";
+		        } else if (type.equals(DocumentEvent.EventType.INSERT)) {
+		          typeString = "Insert";
+		        } else if (type.equals(DocumentEvent.EventType.REMOVE)) {
+		          typeString = "Remove";
+		        }
+		        System.out.print("Type  :   " + typeString + " / ");
+		        Document source = documentEvent.getDocument();
+		        int length = source.getLength();
+		        try {
+		          System.out.println("Contents: " + source.getText(0, length));
+		        } catch (BadLocationException badLocationException) {
+		          System.out.println("Contents: Unknown");
+		        }
+		      }
+		    };
+	      newUserName.getDocument().addDocumentListener(documentListener);
+
+		
+		JLabel passwordLabel = new JLabel("Password: ");
+		newPassword = new JTextField(15);
+		panel2.add(passwordLabel);
+		panel2.add(newPassword);
+
+		
+		panel2.add(ok);
+		panel2.add(cancel);	
+		frame.add(panel);
+		frame.add(panel2);
+		
+
+		infoBox = Box.createVerticalBox();
 		JButton createButton = new JButton("Create a User");
 		createButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -49,9 +208,8 @@ public class zCRM_GUI {
 			}
 		});
 		infoBox.add(createButton);
-		p.add(infoBox);
-			
-		JButton editButton = new JButton("Edit a User     ");
+
+		JButton editButton = new JButton("Edit a User");
 		editButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -61,9 +219,8 @@ public class zCRM_GUI {
 			}
 		});
 		infoBox.add(editButton);
-		p.add(infoBox);
 			
-		JButton deleteButton = new JButton("Delete a User ");
+		JButton deleteButton = new JButton("Delete a User");
 		deleteButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -74,7 +231,7 @@ public class zCRM_GUI {
 		});
 		infoBox.add(deleteButton);
 		
-		JButton logout = new JButton("Logout             ");
+		JButton logout = new JButton("Logout");
 		logout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -85,29 +242,25 @@ public class zCRM_GUI {
 		});
 		infoBox.add(logout);
 
-		p.add(infoBox);
+		userTitle.add(infoBox);
 		frame.setVisible(true);	
 	}	
-	
+	  public void focusGained(FocusEvent e) {
+          System.out.println("Focus gained");
+      } 
+	  
 	private static void createUser() {
 		
 		String userName = "", password = "";
-		
-		userName = JOptionPane.showInputDialog(frame, "Enter Username");
-		password = JOptionPane.showInputDialog(frame, "Enter Password");
 		
 	}
 	
 	private static void editUser() {
 		String userName = "";
-		
-		userName = JOptionPane.showInputDialog(frame, "Enter Username to edit");
 	}
 	
 	private static void deleteUser() {
 		String userName = "";
-		
-		userName = JOptionPane.showInputDialog(frame, "Enter Username to delete");
 	}
 	
 	public static void main(String[] args)
