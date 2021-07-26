ae83cdc085157926e4f9f4694324f8178766b56f
Nicholas Krone <nkrone2@horizon.csueastbay.edu>
2021-07-26 16:17:10 -0700

    remove stub comment

--- src/test/java/edu/cs401group3/crm/storage/StorageManagerTest.java
+++ src/test/java/edu/cs401group3/crm/storage/StorageManagerTest.java
@@ -58,7 +58,6 @@ public class StorageManagerTest {
 		try {
 			TimeUnit.SECONDS.sleep(1);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
